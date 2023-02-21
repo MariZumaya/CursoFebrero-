03d2c3f71b1378fb5778b67891fd2fb56e54efc0
MariZumaya <126021317+MariZumaya@users.noreply.github.com>
2023-02-21 14:46:54 -0600

    Add files via upload

---
+++ LZ Tienda/LZ Tienda/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LZ Tienda
+//
+//  Created by Laboratorio11 UNAM-11 on 14/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
